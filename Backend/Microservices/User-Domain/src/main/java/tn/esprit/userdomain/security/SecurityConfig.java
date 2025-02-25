da8bfd76d2cbb6529f2806761421c4578767802c
FaroukBenhaj <mohamedfarouk.benhajamor@esprit.tn>
2025-02-25 01:42:15 +0100

    Token Entity & Repo / implememnting the security configuration class

---
+++ Backend/Microservices/User-Domain/src/main/java/tn/esprit/userdomain/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package tn.esprit.userdomain.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor // In order to create a constructor with all private or the final fields
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+    private JwtFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable) //csrf  => Cross site resource forgery
+                .authorizeHttpRequests(req -> req.requestMatchers(
+                        //Authorize all the methodes that are in the this list
+                        "/auth/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/swagger-ui.html"
+                ).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                ).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilter(jwtAuthFilter , UsernamePasswordAuthenticationFilter.class)
+
+    }
+}
--- Backend/Microservices/User-Domain/src/main/java/tn/esprit/userdomain/user/UserRepository.java
+++ Backend/Microservices/User-Domain/src/main/java/tn/esprit/userdomain/user/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     //Optional => est une classes generique qui sert a encapsuler un obj present ou absent
-    // Pour eviter NullPointerException 
+    // Pour eviter NullPointerException
     Optional<User> findByEmail(String email);
 }
