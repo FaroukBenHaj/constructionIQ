bf4b0f6105d89b26d053d4739a6a9920cfac61fe
FaroukBenhaj <mohamedfarouk.benhajamor@esprit.tn>
2025-03-06 06:15:13 +0100

    Register Frontend Done

--- Frontend/FrontOffice_ConstructionIQ/src/app/Pages/register/register.component.ts
+++ Frontend/FrontOffice_ConstructionIQ/src/app/Pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {RegistrationRequest} from "../../services/models/registration-request";
+import {Router} from "@angular/router";
+import {AuthentificationService} from "../../services/services/authentification.service";
 
 @Component({
   selector: 'app-register',
@@ -9,4 +11,28 @@ import {RegistrationRequest} from "../../services/models/registration-request";
 export class RegisterComponent {
   registerRequest: RegistrationRequest ={ email:'' , firstName:'' ,lastName:'' , password:''};
   errorMsg: Array<string> =[];
+
+    constructor(
+      private router: Router,
+      private authService: AuthentificationService
+    ) {
+    }
+
+  register_user() {
+  this.errorMsg=[];
+  this.authService.register({
+    body: this.registerRequest
+  }).subscribe({
+    next: ():void => {
+      this.router.navigate(['activate-account']);
+    },
+    error: (err):void =>{
+      this.errorMsg = err.error.validationErrors;
+    }
+  })
+  }
+
+  login() {
+  this.router.navigate(['login'])
+  }
 }
--- Frontend/FrontOffice_ConstructionIQ/src/app/app-routing.module.ts
+++ Frontend/FrontOffice_ConstructionIQ/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import {LoginComponent} from "./Pages/login/login.component";
+import {RegisterComponent} from "./Pages/register/register.component";
 
 const routes: Routes = [
 
   { path : 'home' , component:HomePageComponent},
   { path : '' , redirectTo: '/home' , pathMatch:'full'},
   { path : 'login' , component:LoginComponent},
+  { path : 'register' , component:RegisterComponent},
 
 ];
 
